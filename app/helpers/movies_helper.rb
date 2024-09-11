f6e82d76d089be34f81764585acb84a7204e2bf0
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2024-09-11 16:43:39 -0500

    New Changes in Rotten Potatoes

---
+++ app/helpers/movies_helper.rb
@@ -0,0 +1,9 @@
+module MoviesHelper
+    # Generates a sortable column link.
+    def sortable(column, title = nil)
+      title ||= column.titleize
+      direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
+      link_to title, { sort: column, direction: direction }
+    end
+  end
+  
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Seed the RottenPotatoes DB with some movies.
+more_movies = [
+  {:title => 'My Neighbor Totoro', :rating => 'G',
+    :release_date => '16-Apr-1988'},
+  {:title => 'Green Book', :rating => 'PG-13',
+    :release_date => '16-Nov-2018'},
+  {:title => 'Parasite', :rating => 'R',
+    :release_date => '30-May-2019'},
+  {:title => 'Nomadland', :rating => 'R',
+    :release_date => '19-Feb-2021'},
+  {:title => 'CODA', :rating => 'PG-13',
+    :release_date => '13-Aug-2021'},
+  {:title => 'Deadpool & Wolverine', :rating => 'R',
+    :release_date => '26-July-2024'},
+  {:title => 'Stree 2', :rating => 'A',
+    :release_date => '15-Aug-2024'},
+  {:title => 'Fighter', :rating => 'NR',
+    :release_date => '26-May-2024'},
+  {:title => 'Maidaan', :rating => 'R',
+    :release_date => '10-Mar-2024'} 
+]
+
+more_movies.each do |movie|
+  Movie.create!(movie)
+end
