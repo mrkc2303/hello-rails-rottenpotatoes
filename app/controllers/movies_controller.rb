5f42b363b7c5ec96946c4fd9e4623dd410c96562
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2024-09-11 17:44:15 -0500

    Minor changes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,25 +3,20 @@ class MoviesController < ApplicationController
 
   helper_method :sort_column, :sort_direction
 
-  # GET /movies or /movies.json
   def index
     @movies = Movie.order("#{sort_column} #{sort_direction}")
   end
 
-  # GET /movies/1 or /movies/1.json
   def show
   end
 
-  # GET /movies/new
   def new
     @movie = Movie.new
   end
 
-  # GET /movies/1/edit
   def edit
   end
 
-  # POST /movies or /movies.json
   def create
     @movie = Movie.new(movie_params)
 
@@ -36,7 +31,6 @@ class MoviesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /movies/1 or /movies/1.json
   def update
     respond_to do |format|
       if @movie.update(movie_params)
@@ -49,7 +43,6 @@ class MoviesController < ApplicationController
     end
   end
 
-  # DELETE /movies/1 or /movies/1.json
   def destroy
     @movie.destroy!
 
@@ -60,23 +53,19 @@ class MoviesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_movie
       @movie = Movie.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def movie_params
       params.require(:movie).permit(:title, :rating, :description, :release_date)
     end
 
     def sort_column
-      # Allow sorting by title, rating, or release_date. Default to title.
       %w[title rating release_date].include?(params[:sort]) ? params[:sort] : "title"
     end
 
     def sort_direction
-      # Allow sorting in ascending or descending order. Default to ascending.
       %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,23 +11,23 @@
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
   {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
+    :release_date => '16-Apr-1988', :description => 'Movie Description'},
   {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
+    :release_date => '16-Nov-2018', :description => 'Movie Description'},
   {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
+    :release_date => '30-May-2019', :description => 'Movie Description'},
   {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
+    :release_date => '19-Feb-2021', :description => 'Movie Description'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'},
+    :release_date => '13-Aug-2021', :description => 'Movie Description'},
   {:title => 'Deadpool & Wolverine', :rating => 'R',
-    :release_date => '26-July-2024'},
+    :release_date => '26-July-2024', :description => 'Marvel Entertainment'},
   {:title => 'Stree 2', :rating => 'A',
-    :release_date => '15-Aug-2024'},
+    :release_date => '15-Aug-2024', :description => 'Movie Description'},
   {:title => 'Fighter', :rating => 'NR',
-    :release_date => '26-May-2024'},
+    :release_date => '26-May-2024', :description => '2 people fight'},
   {:title => 'Maidaan', :rating => 'R',
-    :release_date => '10-Mar-2024'} 
+    :release_date => '10-Mar-2024', :description => 'A movie on Football'} 
 ]
 
 more_movies.each do |movie|
