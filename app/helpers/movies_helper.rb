097cdb92dd5dec252909c9a30b666295daae8225
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2024-09-11 18:35:36 -0500

    Added indicator to asc or desc

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,7 +3,15 @@ module MoviesHelper
     def sortable(column, title = nil)
       title ||= column.titleize
       direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
-      link_to title, { sort: column, direction: direction }
+      
+      # Determine the arrow based on the sort direction
+      arrow = if column == params[:sort]
+                params[:direction] == "asc" ? "▲" : "▼"
+              else
+                ""
+              end
+  
+      # Add the arrow next to the title and make it part of the link
+      link_to "#{title} #{arrow}".html_safe, { sort: column, direction: direction }
     end
   end
-  
